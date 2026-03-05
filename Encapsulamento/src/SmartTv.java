0ff0f56aa37674dc2b5c641e2af0aeb2529d393e
MiguelHenriqueO <miguelhenriqueisaac@gmail.com>
2026-03-05 20:30:45 -0300

    encapsulamento aula 5/3

--- Encapsulamento/src/SmartTv.java
+++ Encapsulamento/src/SmartTv.java
@@ -1,15 +1,17 @@
 public class SmartTv {
     private String marca, modelo;
     private int volume;
+    private boolean conectadoInternet;
 
     public SmartTv() {
 
     }
 
     public SmartTv(String marca, String modelo, int volume){
-        this.marca = marca;
-        this.modelo = modelo;
+        this.setMarca(marca);
+        this.setModelo(modelo);
         this.setVolume(volume);
+        this.conectadoInternet = false;
     }
 
 
@@ -27,7 +29,11 @@ public class SmartTv {
 
 
     public void setMarca(String marca){
-        this.marca = marca;
+        if (marca.length() <= 30){
+            this.marca = marca;
+        }else{
+            System.out.println("Marca tem mais de 30 caracteres");
+        }
     }
     public String getMarca(){
         return this.marca;
@@ -35,11 +41,60 @@ public class SmartTv {
 
 
     public void setModelo(String modelo){
-        this.modelo = modelo;
+        //todo modelo deve começar com letra maiúscula
+        char primeiro = modelo.charAt(0);
+        boolean resp = Character.isUpperCase(primeiro);
+        if (resp) {
+            this.modelo = modelo;
+        }
     }
     public String getModelo(){
         return this.modelo;
     }
 
+    public void setConectadoInternet(boolean conectadoInternet){ this.conectadoInternet = conectadoInternet; }
+    public Boolean isConectadoInternet(){ return this.conectadoInternet; }
+
+    //abrir Youtube
+
+    public void abrirYoutube(){
+        if(this.verificaInternet()){
+            System.out.println("Abrindo Youtube");
+        }else{
+            System.out.println("Youtube não pode ser aberto sem internet");
+        }
+    }
+
+
+    //aumentar o volume em x
+    public void aumentarVolume(int x){
+        this.setVolume (this.volume + x);
+
+    }
+
+    //diminuir o volume em x
+    public void diminuirVolume(int x){
+        this.setVolume (this.volume - x);
+    }
+
+    //verifica internet
+    private boolean verificaInternet(){
+        System.out.println("Buscando sinal de Wi-fi...");
+        System.out.println("Verificando Credenciais...");
+        System.out.println("Autenticando IP...");
+
+        // 50% de chance de conectar e 50% de chance de falhar
+
+        int randomico = (int) (Math.random() * 10);
+        if (randomico < 5){
+            System.out.println("Conectou na Internet");
+            return true;
+        }
+        else{
+            System.out.println("Problema na conexão");
+            return false;
+        }
+
+    }
 
 }
--- Encapsulamento/src/TestaSmartTv.java
+++ Encapsulamento/src/TestaSmartTv.java
@@ -7,8 +7,22 @@ public class TestaSmartTv {
 
         System.out.println("Volume de s1: " + s1.getVolume());
 
+        s1.abrirYoutube();
+
         SmartTv s2 = new SmartTv("Samsung","Qled de 45 polegadas",0);
 
+        s2.abrirYoutube();
+
+        s2.aumentarVolume(10);
+        s2.aumentarVolume(110);
+
+        s2.diminuirVolume(200);
+        s2.diminuirVolume(5);
+
+        SmartTv s3 = new SmartTv("LG","WideScreen 65",-30);
+
+        s3.abrirYoutube();
+        System.out.println("Volume de s3: " + s3.getVolume());
     }
 
 }
